16b847659da5fb3fe47130bb5c4b3a19ddb4eed1
RAHUL KUMAR VERMA <44740032+variableverma@users.noreply.github.com>
2023-07-05 13:16:57 +0530

    Update helloworld2.java
    change made 2nd time to see poll scm concept

--- helloworld2.java
+++ helloworld2.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!......1st change applied"); 
+        System.out.println("-----change made 2nd time----"); 
     }
 }
